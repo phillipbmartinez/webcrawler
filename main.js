acae04e0ffb1c89cfaab3d7b2be0313312f61712
Phillip Martinez <phillip.martinez210@gmail.com>
2024-06-17 09:16:06 -0500

    Complete web crawler files

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+import { crawlPage } from './crawl.js'
+import { printReport } from './report.js'
+
+async function main() {
+  if (process.argv.length < 3) {
+    console.log('no website provided')
+    return
+  }
+  if (process.argv.length > 3) {
+    console.log('too many arguments provided')
+    return
+  }
+  const baseURL = process.argv[2]
+
+  console.log(`starting crawl of: ${baseURL}...`)
+
+  const pages = await crawlPage(baseURL)
+
+  printReport(pages)
+}
+
+main()
